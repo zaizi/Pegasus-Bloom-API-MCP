4eb023f9c832b21314198d390abd41a78b0f8109
KAdenrele <akomolafe@zaizi.com>
2025-11-25 10:17:33 +0000

    database.py file updated with condition to look for DBPASS which is used in our AWS environment

--- db/database.py
+++ db/database.py
@@ -5,15 +5,18 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-mysql_username = os.getenv("POSTGRES_USER")
-mysql_password = os.getenv("DB_PASSWORD")
-mysql_database= os.getenv("POSTGRES_DB")
+db_username = os.getenv("POSTGRES_USER")
+db_password = os.getenv("DB_PASSWORD")
+database_name= os.getenv("POSTGRES_DB")
 db_host = os.getenv("DB_HOST", "db")
+DBPASS = os.getenv("DBPASS", None)
 
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-if DATABASE_URL is None:
-    raise ValueError("DATABASE_URL environment variable is not set")
+if DBPASS is None:
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    if DATABASE_URL is None:
+        raise ValueError("DATABASE_URL environment variable is not set")
+else:
+    DATABASE_URL = f"postgresql+psycopg2://{db_username}:{DBPASS}@{db_host}:5432/{database_name}"
 
 # Create the SQLAlchemy engine
 engine = create_engine(DATABASE_URL)
