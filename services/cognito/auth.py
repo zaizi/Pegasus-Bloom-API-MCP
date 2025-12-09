7c16e89982535a513e89f2f7a0fa3783309dfaa6
KAdenrele <akomolafe@zaizi.com>
2025-12-09 16:05:19 +0000

    moved cognito defaults into env file

--- services/cognito/auth.py
+++ services/cognito/auth.py
@@ -1,13 +1,13 @@
 from fastapi_cloudauth.cognito import Cognito
 from dotenv import load_dotenv
 import logging
-
+from os import getenv
 load_dotenv()
 logger = logging.getLogger(__name__)
 
-aws_region = "us-east-2"
-userPoolId = ""
-client_id = ""
+aws_region = getenv(key="aws_region", default="eu-east-2")
+userPoolId = getenv(key="userPoolId", default=None)
+client_id = getenv(key="client_id", default=None)
 
 auth = Cognito(
     region=aws_region, 
